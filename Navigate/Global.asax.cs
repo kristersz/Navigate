9202a00fe528d28ce48615c62564a92f704061fd
Kristers <kristers.zimecs@gmail.com>
2013-05-10 03:10:40 +0300

    Started configuring Quartz.NET for sending mail

--- Navigate/Global.asax.cs
+++ Navigate/Global.asax.cs
@@ -1,7 +1,11 @@
 ﻿using MvcHtmlHelpers;
 using Navigate.Models.Classifiers;
+using Navigate.Quartz.Jobs;
+using Quartz;
+using Quartz.Impl;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -28,6 +32,44 @@ namespace Navigate
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
+
+            ConfigureQuartzJobs();
         }
+
+        public static void ConfigureQuartzJobs()
+        {
+            var properties = new NameValueCollection();
+            properties["quartz.scheduler.instanceName"] = "ServerScheduler";
+
+            // set thread pool info
+            properties["quartz.threadPool.type"] = "Quartz.Simpl.SimpleThreadPool, Quartz";
+            properties["quartz.threadPool.threadCount"] = "5";
+            properties["quartz.threadPool.threadPriority"] = "Normal";
+
+            // set remoting expoter
+            properties["quartz.scheduler.proxy"] = "true";
+            properties["quartz.scheduler.proxy.address"] = string.Format("tcp://localhost:555/QuartzScheduler");
+            // construct a scheduler factory
+            ISchedulerFactory schedFact = new StdSchedulerFactory();
+
+            // get a scheduler
+            IScheduler sched = schedFact.GetScheduler();
+            sched.Start();
+
+            // construct job info
+            IJobDetail jobDetail = JobBuilder.Create<HelloJob>()
+                .WithIdentity("myJob", "myGroup")
+                .RequestRecovery()
+                .Build();
+
+            //created trigger which will fire every minute starting immediately
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("myTrigger", "myTriggerGroup")
+                .StartNow()
+                .WithSimpleSchedule(x => x.WithIntervalInSeconds(10).WithRepeatCount(5))
+                .Build();
+                    
+            sched.ScheduleJob(jobDetail, trigger);
+        }      
     }
 }
---
+++ Navigate/Quartz/Jobs/HelloJob.cs
@@ -0,0 +1,23 @@
+﻿using Common.Logging;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace Navigate.Quartz.Jobs
+{
+    public class HelloJob : IJob
+    {
+
+        public HelloJob()
+        { 
+        }
+
+        public void Execute(IJobExecutionContext context)
+        {
+            Debug.WriteLine("Hello at " + DateTime.Now.ToString());
+        }
+    }
+}
